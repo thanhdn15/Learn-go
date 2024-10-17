11e762bfed373094b241cc982391d33b7950a40a
thanhdn15 <thanhdn@concrete-corp.com>
2024-10-17 10:28:41 +0700

    interface

--- interface/main.go
+++ interface/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Test struct {
 	t string
 }
@@ -16,6 +18,54 @@ func (t Test) Demo() string {
 	return "abc"
 }
 
-func main() {
+type Speaker interface {
+	Speak() string
+}
 
+type Foo struct {
+}
+
+func (Foo) Speak() string {
+	return "Hello, I'm foo"
+}
+
+func describe(i interface{}) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
+
+func main() {
+	var someSpeaker Speaker = Foo{}
+	fmt.Print(someSpeaker.Speak())
+
+	var i interface{}
+	describe(i)
+
+	i = 42
+	describe(i)
+
+	i = "Hello"
+	describe(i)
+
+	product := make(map[string]interface{}, 0)
+
+	product["name"] = "Iphone 13 Pro max"
+	product["price"] = 31000000
+	product["quantity"] = 40
+
+	fmt.Print(product)
+
+	// Ép kiểu dữ liệu Interface
+	var m interface{} = "hello"
+
+	s := m.(string)
+	fmt.Println(s)
+
+	s, ok := m.(string)
+	fmt.Println(s, ok)
+
+	f, ok := m.(float64)
+	fmt.Println(f, ok)
+
+	//f = m.(float64) // panic
+	//fmt.Println(f)
 }
